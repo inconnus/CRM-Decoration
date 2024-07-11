e44e4b72a87fa250097a9e50f1de7549fb37043c
inconnus <chindanai.mal@gmail.com>
2024-07-11 13:38:26 +0700

    //

--- providers/dataslot.tsx
+++ providers/dataslot.tsx
@@ -8,10 +8,10 @@ const DataslotProvider = ({ children }: { children: React.ReactNode }) => {
     const [, setLine] = useLine()
     const [, setReady] = useReady()
     useInitial(async () => {
-        // await liff.init({ liffId: config?.liff[process.env.NEXT_PUBLIC_VERCEL_ENV || ''] })
-        // if (!liff.isLoggedIn()) return liff.login()
-        // const _token = liff.getAccessToken()
-        // setLine({ ...await liff.getProfile(), token: _token })
+        await liff.init({ liffId: '1661500410-PRG7qjRQ' })
+        if (!liff.isLoggedIn()) return liff.login()
+        const _token = liff.getAccessToken()
+        setLine({ ...await liff.getProfile(), token: _token })
         setReady(true)
     }, [config])
     return children
